c68e8d630c1f66537932bff79484f74177fce2b1
marcorennmaus <17mh2000@gmail.com>
2015-08-18 22:02:58 +0200

    Updated script.js to version alpha-v0.3

--- script.js
+++ script.js
@@ -1,73 +1,113 @@
-var count = 0;
+var bytes = 0;
+var kilobytes = 0;
+var totalCount = 0;
 var level = 1;
 
-document.getElementById("Counter").innerHTML = count;
+document.getElementById("Counter").innerHTML = kilobytes + " KB - " + bytes + " Bytes";
+
+function convertBytesKilobytes()
+	{
+	bytes = bytes - 1024
+	kilobytes = kilobytes + 1
+	}
+	
+function convertKilobytesBytes()
+	{
+	bytes = bytes + 1024
+	kilobytes = kilobytes - 1
+	}
+
+function sixteenBitEgg()
+	{
+	console.log("ERROR: 16-Bit-Number-Overflow. Bytes have been reset to 0.");
+	kilobytes = 0
+	bytes = 0
+	}
 
 function upCount(){
- document.getElementById("Counter").innerHTML = "Bytes: " + count;
+ document.getElementById("Counter").innerHTML = kilobytes + " KB - " + bytes + " Bytes";
  document.getElementById("level").innerHTML = "Level: " + level;
- count += 1;
+ bytes += 1;
+ totalCount += 1;
  //console.log(count);
-if (level === 1 && count >= 25)
+  if (bytes >= 1024)
 	{
-	level = 2
-	console.log("Congratulations! You leveled up to Level 2!");
+	convertBytesKilobytes()
 	}
 else
 	{
-	if (level === 2 && count >= 50)
+	if (bytes < 0)
 		{
-		level = 3
-		console.log("Congratulations! You leveled up to Level 3!");
+		convertKilobytesBytes()
+		}
+	}
+ if (bytes >= 65536)
+	{
+	sixteenBitEgg();
+	}
+else
+	{
+	if (level === 1 && totalCount >= 25)
+		{
+		level = 2
+		console.log("Congratulations! You leveled up to Level 2!");
 		}
 	else
 		{
-		if (level === 3 && count >= 100)
+		if (level === 2 && totalCount >= 50)
 			{
-			level = 4
-			console.log("Congratulations! You leveled up to Level 4!");
+			level = 3
+			console.log("Congratulations! You leveled up to Level 3!");
 			}
 		else
 			{
-			if (level === 4 && count >= 250)
+			if (level === 3 && totalCount >= 100)
 				{
-				level = 5
-				console.log("Congratulations! You leveled up to Level 5!");
+				level = 4
+				console.log("Congratulations! You leveled up to Level 4!");
 				}
 			else
 				{
-				if (level === 5 && count >= 500)
+				if (level === 4 && totalCount >= 250)
 					{
-					level = 6
-					console.log("Congratulations! You leveled up to Level 6!");
+					level = 5
+					console.log("Congratulations! You leveled up to Level 5!");
 					}
 				else
 					{
-					if (level === 6 && count >= 800)
+					if (level === 5 && totalCount >= 500)
 						{
-						level = 7
-						console.log("Congratulations! You leveled up to Level 7!");
+						level = 6
+						console.log("Congratulations! You leveled up to Level 6!");
 						}
 					else
 						{
-						if (level === 7 && count >= 1200)
+						if (level === 6 && totalCount >= 800)
 							{
-							level = 8
-							console.log("Congratulations! You leveled up to Level 8!");
+							level = 7
+							console.log("Congratulations! You leveled up to Level 7!");
 							}
 						else
 							{
-							if (level === 8 && count >= 1850)
+							if (level === 7 && totalCount >= 1200)
 								{
-								level = 9
-								console.log("Congratulations! You leveled up to Level 9!");
+								level = 8
+								console.log("Congratulations! You leveled up to Level 8!");
 								}
 							else
 								{
-								if (level === 9 && count >= 2500)
+								if (level === 8 && totalCount >= 1850)
 									{
-									level = 10
-									console.log("Congratulations! You leveled up to Level 10!");
+									level = 9
+									console.log("Congratulations! You leveled up to Level 9!");
+									}
+								else
+									{
+									if (level === 9 && totalCount >= 2500)
+										{
+										level = 10
+										console.log("Congratulations! You leveled up to Level 10!");
+										}
 									}
 								}
 							}
